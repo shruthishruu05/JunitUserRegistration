e3d0b53d98a6c251e4e11305967d9c1c29d3db1c
shruthishruu05 <shruthi.m@perfios.com>
2021-09-10 15:30:06 +0530

    REFACTORED : uservalidation using throws ExceptionUserValidation

--- src/main/java/com/bridgelabz/junituserregistration/UserValidation.java
+++ src/main/java/com/bridgelabz/junituserregistration/UserValidation.java
@@ -7,8 +7,8 @@ public class UserValidation {
 			if (name.length() == 0) {
 				throw new ExceptionUserValidation("Enter Valid Name, the name entered is empty!!.");
 			}
-			String nameRegex = "^[A-Z]{1}[a-z]{2,}$";
-			return name.matches(nameRegex);
+			String nameExpression = "^[A-Z]{1}[a-z]{2,}$";
+			return name.matches(nameExpression);
 			
 		} 
 		catch (NullPointerException e) {
@@ -23,8 +23,8 @@ public class UserValidation {
 			if (email.length() == 0) {
 				throw new ExceptionUserValidation("Please Enter Valid Email Address as it is empty");
 			}
-			String emailRegex = "^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$";
-			return email.matches(emailRegex);
+			String emailExpression = "^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$";
+			return email.matches(emailExpression);
 		} 
 		catch (NullPointerException e) {
 			throw new ExceptionUserValidation("Please Enter Valid Email Address as it is null");
@@ -38,8 +38,8 @@ public class UserValidation {
 			if (mobileNumber.length() == 0) {
 				throw new ExceptionUserValidation("Please Enter Valid Mobile Number as it is empty");			
 			}
-			String mobileNumberRegex = "^[0-9]{1,2}\\s{1}[0-9]{10}$";
-			return mobileNumber.matches(mobileNumberRegex);
+			String mobileNumberExpression = "^[0-9]{1,2}\\s{1}[0-9]{10}$";
+			return mobileNumber.matches(mobileNumberExpression);
 		} 
 		catch (NullPointerException e) {
 			throw new ExceptionUserValidation("Please Enter Valid Mobile Number as it is null");
@@ -52,8 +52,8 @@ public class UserValidation {
 			if (password.length() == 0) {
 				throw new ExceptionUserValidation("Please Enter Valid Password ,the password entered is empty ");
 			}
-			String passwordRegex = "^(?=.*[A-Z])(?=.*[0-9])(?=.{8,}$)[a-zA-Z0-9]*[@#$%^&-+=()][a-zA-Z0-9]*$";
-			return password.matches(passwordRegex);
+			String passwordExpression = "^(?=.*[A-Z])(?=.*[0-9])(?=.{8,}$)[a-zA-Z0-9]*[@#$%^&-+=()][a-zA-Z0-9]*$";
+			return password.matches(passwordExpression);
 
 		} catch (NullPointerException e) {
 			throw new ExceptionUserValidation("Please Enter Valid Password, the password entered is null.");
