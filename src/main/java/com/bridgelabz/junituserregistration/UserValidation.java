8fc87c3c65ad6d7a58d8d4835bd7efe7ef1e7570
shruthishruu05 <shruthi.m@perfios.com>
2021-09-10 15:19:37 +0530

    REFACTORED : method to validate user details

--- src/main/java/com/bridgelabz/junituserregistration/UserValidation.java
+++ src/main/java/com/bridgelabz/junituserregistration/UserValidation.java
@@ -1,26 +1,64 @@
 package com.bridgelabz.junituserregistration;
 
 public class UserValidation {
-	public boolean validateName(String name) 
-	{
-		String nameRegex = "^[A-Z]{1}[a-z]{2,}$";
-		return name.matches(nameRegex);
-	}
-	public boolean validateEmail(String email) 
-	{
+	public boolean validateName(String name) throws ExceptionUserValidation {
 
-		String emailRegex = "^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$";
-		return email.matches(emailRegex);
+		try {
+			if (name.length() == 0) {
+				throw new ExceptionUserValidation("Enter Valid Name, the name entered is empty!!.");
+			}
+			String nameRegex = "^[A-Z]{1}[a-z]{2,}$";
+			return name.matches(nameRegex);
+			
+		} 
+		catch (NullPointerException e) {
+			throw new ExceptionUserValidation("Enter Valid Name, the name entered is null!!.");
+		}
 	}
 	
-	public boolean validateMobileNumber(String mobileNumber) {
 
-		String mobileNumberRegex = "^[0-9]{1,2}\\s{1}[0-9]{10}$";
-		return mobileNumber.matches(mobileNumberRegex);
-	}
-	public boolean validatePassword(String password) {
+	public boolean validateEmail(String email) throws ExceptionUserValidation {
 
-		String passwordRegex = "^(?=.*[A-Z])(?=.*[0-9])(?=.{8,}$)[a-zA-Z0-9]*[@#$%^&-+=()][a-zA-Z0-9]*$";
-		return password.matches(passwordRegex);
+		try {
+			if (email.length() == 0) {
+				throw new ExceptionUserValidation("Please Enter Valid Email Address as it is empty");
+			}
+			String emailRegex = "^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$";
+			return email.matches(emailRegex);
+		} 
+		catch (NullPointerException e) {
+			throw new ExceptionUserValidation("Please Enter Valid Email Address as it is null");
+
+		}
 	}
+
+	public boolean validateMobileNumber(String mobileNumber) throws ExceptionUserValidation {
+
+		try {
+			if (mobileNumber.length() == 0) {
+				throw new ExceptionUserValidation("Please Enter Valid Mobile Number as it is empty");			
+			}
+			String mobileNumberRegex = "^[0-9]{1,2}\\s{1}[0-9]{10}$";
+			return mobileNumber.matches(mobileNumberRegex);
+		} 
+		catch (NullPointerException e) {
+			throw new ExceptionUserValidation("Please Enter Valid Mobile Number as it is null");
+		}
+	}
+
+	public boolean validatePassword(String password) throws ExceptionUserValidation {
+
+		try {
+			if (password.length() == 0) {
+				throw new ExceptionUserValidation("Please Enter Valid Password ,the password entered is empty ");
+			}
+			String passwordRegex = "^(?=.*[A-Z])(?=.*[0-9])(?=.{8,}$)[a-zA-Z0-9]*[@#$%^&-+=()][a-zA-Z0-9]*$";
+			return password.matches(passwordRegex);
+
+		} catch (NullPointerException e) {
+			throw new ExceptionUserValidation("Please Enter Valid Password, the password entered is null.");
+		}	
+	}
+	
+
 }
