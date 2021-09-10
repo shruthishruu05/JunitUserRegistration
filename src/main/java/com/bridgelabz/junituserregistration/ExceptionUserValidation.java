4be22ce194ed47ec859896971da072e8ed694e15
shruthishruu05 <shruthi.m@perfios.com>
2021-09-10 15:25:40 +0530

    ADDED : Exception class to take custom exception

---
+++ src/main/java/com/bridgelabz/junituserregistration/ExceptionUserValidation.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.junituserregistration;
+
+public class ExceptionUserValidation extends Exception {
+	public ExceptionUserValidation(String message) {
+
+		super(message);
+		
+	}
+
+}
