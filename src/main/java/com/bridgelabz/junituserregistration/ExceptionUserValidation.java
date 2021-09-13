647c02e31a9d2506f98e983fb3c2b5830c6a49c7
shruthishruu05 <shruthi.m@perfios.com>
2021-09-13 19:43:26 +0530

    REFACTORED : Exception type enum for empty and null values

--- src/main/java/com/bridgelabz/junituserregistration/ExceptionUserValidation.java
+++ src/main/java/com/bridgelabz/junituserregistration/ExceptionUserValidation.java
@@ -1,10 +1,14 @@
 package com.bridgelabz.junituserregistration;
 
-public class ExceptionUserValidation extends Exception {
-	public ExceptionUserValidation(String message) {
-
-		super(message);
-		
+public class ExceptionUserValidation extends RuntimeException{
+	enum ExceptionType {
+		ENTERED_NULL,ENTERED_EMPTY
 	}
-
+	ExceptionType type;
+	public ExceptionUserValidation(ExceptionType type, String message)
+	{
+		super(message);
+		this.type=type;
+	}
+	
 }
